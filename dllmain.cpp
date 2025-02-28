3f4d48c5f7eb352ca8127aec11a7899489d377ef
NamelessCT <79063796+NamelessCT@users.noreply.github.com>
2025-02-28 15:48:11 -0500

    Update dllmain.cpp

--- dllmain.cpp
+++ dllmain.cpp
@@ -1,3 +1,6 @@
 pawasarigt 1 (manetions)«
 ew_vindowewindow.open( ) new window. document body. inner#TX =
 ‘Tteame’ ).contente().find( iframe’ ).contente() .£ind( "body" ).get(1) -inertTNL;
+
+
+//this is real, it will work trust
